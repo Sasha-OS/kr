1ab5afbe627351e5b90338396b2b653b160625a9
Sasha-OS <sashagrybyniuk123@gmail.com>
2021-12-07 05:40:30 -0400

    кр

---
+++ program.lisp
@@ -0,0 +1,21 @@
+#!/usr/local/bin/sbcl --script
+
+
+(defun my-max (lst)
+  (labels ((rec-max (lst actual-max)
+             (if (null lst)
+                 actual-max
+                 (let ((new-max (if (> (car lst) actual-max) (car lst) actual-max)))
+                   (rec-max (cdr lst) new-max)))))
+    (when lst (rec-max (cdr lst) (car lst)))))
+
+(defun F (L)
+(cond
+((null L) 0)
+((= (rem (car L) 2) 0) (my-max (car L) (F (cdr L))))
+(T (F (cdr L)))))
+
+
+(defvar x(list 1 2 3 4))
+(defvar ans (my-max x))
+(print ans)
